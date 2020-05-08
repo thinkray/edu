976661db81bcdc8cf8ebe4534fe16bd7a1fa1a6d
thinkray <3523177+thinkray@users.noreply.github.com>
2020-05-08 19:24:11 +0800

    Implement the LogListAPI and the LogAPI

--- site_log/api_urls.py
+++ site_log/api_urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.LogListAPI.as_view(), name='log_list_api'),
-    path('<int:log_id>/', views.LogAPI.as_view(), name='log_api'),
+    path('list', views.LogListAPI.as_view(), name='log_list_api'),
+    path('<int:log_id>', views.LogAPI.as_view(), name='log_api'),
 ]
--- site_log/views.py
+++ site_log/views.py
@@ -1,22 +1,147 @@
 from django.views import View
 from django.shortcuts import render
+from django.utils.timezone import localtime, now
+from django.forms import Form, IntegerField, MultipleChoiceField, CharField
+from account.models import User
+from .models import Log
 from django.http import JsonResponse
-
+import json
 
 class LogListAPI(View):
     def get(self, request):
-        return JsonResponse({
-            'error': 'NotImplementedError'
-        })
+        class LogListAPIGetForm(Form):
+            offset = IntegerField(initial=1, required=False)
+            limit = IntegerField(initial=10, required=False)
+            choices = (
+                ("user", "user"),
+                ("date", "date"),
+                ("operation", "operation"),
+            )
+            column = MultipleChoiceField(choices=choices)
+
+        try:
+            data = json.loads(request.body)
+
+        except:
+            return JsonResponse({
+                'status': 400,
+                'message': 'JSONDecodeError'
+            }, status=400)
+
+        form = LogListAPIGetForm(data)
+        if form.is_valid():
+            cleaned_data = form.clean()
+
+            if cleaned_data['offset'] is None:
+                cleaned_data['offset'] = 0
+            if cleaned_data['limit'] is None:
+                cleaned_data['limit'] = 10
+
+            result = list(Log.objects.all()[
+                          cleaned_data['offset']:cleaned_data['offset']+cleaned_data['limit']].values('id', *cleaned_data['column']))
+
+            if 'date' in cleaned_data['column']:
+                for each in result:
+                    each['date'] = localtime(each['date'])
+
+            return JsonResponse({
+                'status': 200,
+                'message': 'Success',
+                'data': result,
+            })
+        else:
+            return JsonResponse({
+                'status': 400,
+                'message': form.errors
+            }, status=400)
 
     def post(self, request):
-        return JsonResponse({
-            'error': 'NotImplementedError'
-        })
+        class LogListAPIPostForm(Form):
+            user = IntegerField()
+            operation = CharField()
+
+        try:
+            data = json.loads(request.body)
+
+        except:
+            return JsonResponse({
+                'status': 400,
+                'message': 'JSONDecodeError'
+            }, status=400)
+
+        form = LogListAPIPostForm(data)
+        if form.is_valid():
+            cleaned_data = form.clean()
+
+            try:
+                cleaned_data['user'] = User.objects.get(
+                    pk=cleaned_data['user'])
+            except Exception as e:
+                return JsonResponse({
+                    'status': 400,
+                    'message': 'UserNotFound',
+                }, status=400)
+
+            log = Log(user=cleaned_data['user'], date=now(), operation=cleaned_data['operation'])
+
+            log.save()
+            return JsonResponse({
+                'status': 200,
+                'message': 'Success'
+            })
+        else:
+            return JsonResponse({
+                'status': 400,
+                'message': form.errors
+            }, status=400)
 
 
 class LogAPI(View):
     def get(self, request, log_id):
-        return JsonResponse({
-            'error': 'NotImplementedError'
-        })
+        class LogAPIGetForm(Form):
+            choices = (
+                ("user", "user"),
+                ("date", "date"),
+                ("operation", "operation"),
+            )
+            column = MultipleChoiceField(choices=choices)
+
+        try:
+            data = json.loads(request.body)
+
+        except:
+            return JsonResponse({
+                'status': 400,
+                'message': 'JSONDecodeError'
+            }, status=400)
+
+        form = LogAPIGetForm(data)
+        if form.is_valid():
+            cleaned_data = form.clean()
+
+            try:
+                result = list(Log.objects.filter(pk=log_id).values(
+                    'id', *cleaned_data['column']))
+            except Exception as e:
+                return JsonResponse({
+                    'status': 404,
+                    'message': 'Not Found',
+                }, status=404)
+
+            if result == []:
+                return JsonResponse({
+                    'status': 404,
+                    'message': 'Not Found',
+                    'data': [],
+                }, status=404)
+
+            return JsonResponse({
+                'status': 200,
+                'message': 'Success',
+                'data': result,
+            })
+        else:
+            return JsonResponse({
+                'status': 400,
+                'message': form.errors
+            }, status=400)
