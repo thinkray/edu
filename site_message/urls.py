3736416b6fdc3a98ef47bdbfdf479ff0ef131a78
thinkray <3523177+thinkray@users.noreply.github.com>
2020-05-15 17:28:26 +0800

    Implement the message function

---
+++ site_message/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.views.generic.base import RedirectView
+
+from . import views
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='inbox'), name='message_redirect_view'),
+    path('send', views.MessageSendView.as_view(), name='message_send_view'),
+    path('<int:message_id>', views.MessageDetailView.as_view(), name='message_detail_view'),
+    path('<str:box_name>', views.MessageView.as_view(), name='message_view_box'),
+    path('<str:box_name>/<int:page>', views.MessageView.as_view(), name='message_view_page'),
+]
--- site_message/views.py
+++ site_message/views.py
@@ -1,9 +1,14 @@
 import json
+from datetime import datetime
 
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.forms import (BooleanField, CharField, ChoiceField, DateTimeField,
                           Form, IntegerField, MultipleChoiceField)
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
+from django.template import loader
+from django.urls import reverse
 from django.utils.timezone import localtime, now
 from django.views import View
 
@@ -220,10 +225,15 @@ class MessageAPI(View):
                 query_data.append('sender')
             if not 'recipient' in cleaned_data['column']:
                 query_data.append('recipient')
+            if not 'is_deleted_by_sender' in cleaned_data['column']:
+                query_data.append('is_deleted_by_sender')
+            if not 'is_deleted_by_recipient' in cleaned_data['column']:
+                query_data.append('is_deleted_by_recipient')
 
             try:
-                result = list(Message.objects.filter(pk=message_id).values(
-                    'id', *query_data))
+                message = Message.objects.filter(pk=message_id)
+                message_instance = message[0]
+                result = list(message.values('id', *query_data))
             except Exception as e:
                 return JsonResponse({
                     'status': 404,
@@ -237,7 +247,11 @@ class MessageAPI(View):
                     'data': [],
                 }, status=404)
 
-            if result[0]['sender'] != request.user.id and result[0]['recipient'] != request.user.id:
+            if result[0]['recipient'] == request.user.id and result[0]['is_deleted_by_recipient'] == False:
+                if message_instance.is_unread:
+                    message_instance.is_unread = False
+                    message_instance.save()
+            elif not (result[0]['sender'] == request.user.id and result[0]['is_deleted_by_sender'] == False):
                 return JsonResponse({
                     'status': 403,
                     'message': 'Forbidden'
@@ -250,6 +264,14 @@ class MessageAPI(View):
             if not 'recipient' in cleaned_data['column']:
                 for each in result:
                     del each['recipient']
+
+            if not 'is_deleted_by_sender' in cleaned_data['column']:
+                for each in result:
+                    del each['is_deleted_by_sender']
+
+            if not 'is_deleted_by_recipient' in cleaned_data['column']:
+                for each in result:
+                    del each['is_deleted_by_recipient']
 
             if 'send_date' in cleaned_data['column']:
                 for each in result:
@@ -281,19 +303,19 @@ class MessageAPI(View):
                 'message': 'Not Found',
             }, status=404)
 
-        if message.sender == request.user:
-            message.is_deleted_by_sender = True
-            message.save()
-
-        elif message.recipient == request.user:
-            message.is_deleted_by_recipient = True
-            message.save()
-
-        else:
+        if message.sender != request.user and message.recipient != request.user:
             return JsonResponse({
                 'status': 403,
                 'message': 'Forbidden'
             }, status=403)
+
+        if message.sender == request.user:
+            message.is_deleted_by_sender = True
+            message.save()
+
+        if message.recipient == request.user:
+            message.is_deleted_by_recipient = True
+            message.save()
 
         if message.is_deleted_by_sender and message.is_deleted_by_recipient:
             message.delete()
@@ -302,3 +324,134 @@ class MessageAPI(View):
             'status': 200,
             'message': 'Success',
         })
+
+
+class MessageView(View):
+
+    def get(self, request, box_name='inbox', page=1):
+        if not request.user.is_authenticated:
+            response = redirect(reverse('user_login_view'))
+            response['Location'] += '?redirect_uri=' + request.path
+            return response
+
+        context = {}
+        result = Message.objects.none()
+        if box_name == 'inbox':
+            result = Message.objects.filter(
+                recipient=request.user, is_deleted_by_recipient=False)
+            context['page_name'] = 'Message Inbox'
+
+        elif box_name == 'outbox':
+            result = Message.objects.filter(
+                sender=request.user, is_deleted_by_sender=False)
+            context['page_name'] = 'Message Outbox'
+
+        paginator = Paginator(result, 10)  # Show 25 contacts per page.
+        page_obj = paginator.get_page(page)
+
+        template = loader.get_template('site_message/index.html')
+        
+        context['site_name'] = settings.SITE_NAME
+        context['is_authenticated'] = True
+        context['is_superuser'] = request.user.is_superuser
+        context['is_teacher'] = request.session.get('is_teacher')
+        context['username'] = request.user.name
+        context['box_name'] = box_name
+        context['hide_welcome'] = True
+        context['page_obj'] = page_obj
+
+        page_start = page_obj.number
+        page_bar_num = 5
+        for i in range(int(page_bar_num/2)):
+            if page_start-1 > 0:
+                page_start = page_start - 1
+        page_end = page_start
+        for i in range(int(page_bar_num/2)*2):
+            if page_end+1 <= page_obj.paginator.num_pages:
+                page_end = page_end + 1
+        if page_end-page_start < int(page_bar_num/2)*2:
+            for i in range(int(page_bar_num/2)*2):
+                if page_start-1 >= 1 and page_end-page_start < int(page_bar_num/2)*2:
+                    page_start = page_start - 1
+        page_bar = []
+        for i in range(page_start, page_end+1):
+            page_bar.append(i)
+        context['page_bar'] = page_bar
+
+        return HttpResponse(template.render(context, request))
+
+
+class MessageDetailView(View):
+
+    def get(self, request, message_id):
+        if not request.user.is_authenticated:
+            response = redirect(reverse('user_login_view'))
+            response['Location'] += '?redirect_uri=' + request.path
+            return response
+
+        messsage_api = MessageAPI()
+        request._body = json.dumps({
+            "column": ["title", "send_date", "sender", "recipient", "content", "is_unread"]
+        })
+        result = json.loads(messsage_api.get(request, message_id).content)
+
+        context = {}
+        context['site_name'] = settings.SITE_NAME
+        context['is_authenticated'] = True
+        context['is_superuser'] = request.user.is_superuser
+        context['is_teacher'] = request.session.get('is_teacher')
+        context['username'] = request.user.name
+        context['hide_welcome'] = True
+
+        if 'status' in result:
+            if result['status'] == 200 and 'data' in result:
+                if result['data']:
+                    context['status'] = 200
+                    context['message_id'] = message_id
+                    context['title'] = result['data'][0]['title']
+                    context['send_date'] = datetime.strptime(
+                        result['data'][0]['send_date'], '%Y-%m-%dT%H:%M:%S.%f%z')
+                    context['sender'] = User.objects.filter(
+                        pk=result['data'][0]['sender'])[0]
+                    context['recipient'] = User.objects.filter(
+                        pk=result['data'][0]['recipient'])[0]
+                    if context['sender'] == request.user:
+                        context['box_name'] = 'outbox'
+                        context['page_name'] = 'Message Outbox'
+                    else:
+                        context['box_name'] = 'inbox'
+                        context['page_name'] = 'Message Inbox'
+                    context['content'] = result['data'][0]['content']
+                    context['is_unread'] = result['data'][0]['is_unread']
+            else:
+                context['status'] = result['status']
+                if 'message' in result:
+                    context['message'] = result['message']
+        else:
+            context['status'] = 500
+
+        template = loader.get_template('site_message/detail.html')
+
+        return HttpResponse(template.render(context, request))
+
+
+class MessageSendView(View):
+
+    def get(self, request):
+        if not request.user.is_authenticated:
+            response = redirect(reverse('user_login_view'))
+            response['Location'] += '?redirect_uri=' + request.path
+            return response
+
+        context = {}
+        context['site_name'] = settings.SITE_NAME
+        context['is_authenticated'] = True
+        context['is_superuser'] = request.user.is_superuser
+        context['is_teacher'] = request.session.get('is_teacher')
+        context['username'] = request.user.name
+        context['page_name'] = 'Send a Message'
+        context['hide_welcome'] = True
+        
+        template = loader.get_template('site_message/send.html')
+
+        return HttpResponse(template.render(context, request))
