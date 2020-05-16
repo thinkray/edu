9809b16da581587d6580e0ce23315828d4e01f92
thinkray <3523177+thinkray@users.noreply.github.com>
2020-05-16 16:29:04 +0800

    Fix a permission bug of the UserLogListView

--- dashboard/views.py
+++ dashboard/views.py
@@ -166,50 +166,6 @@ class AdminUserListView(View):
 
         return HttpResponse(template.render(context, request))
 
-class UserLogListView(View):
-
-    def get(self, request, page=1):
-        if not request.user.is_authenticated:
-            response = redirect(reverse('user_login_view'))
-            response['Location'] += '?redirect_uri=' + request.path
-            return response
-
-        context = {}
-        result = Log.objects.all()
-        context['page_name'] = 'Log List'
-
-        paginator = Paginator(result, 10)
-        page_obj = paginator.get_page(page)
-
-        template = loader.get_template('dashboard/user/log_list.html')
-
-        context['site_name'] = settings.SITE_NAME
-        context['is_authenticated'] = True
-        context['is_superuser'] = request.user.is_superuser
-        context['is_teacher'] = request.session.get('is_teacher')
-        context['username'] = request.user.name
-        context['hide_welcome'] = True
-        context['page_obj'] = page_obj
-
-        page_start = page_obj.number
-        page_bar_num = 5
-        for i in range(int(page_bar_num / 2)):
-            if page_start - 1 > 0:
-                page_start = page_start - 1
-        page_end = page_start
-        for i in range(int(page_bar_num / 2) * 2):
-            if page_end + 1 <= page_obj.paginator.num_pages:
-                page_end = page_end + 1
-        if page_end - page_start < int(page_bar_num / 2) * 2:
-            for i in range(int(page_bar_num / 2) * 2):
-                if page_start - 1 >= 1 and page_end - page_start < int(page_bar_num / 2) * 2:
-                    page_start = page_start - 1
-        page_bar = []
-        for i in range(page_start, page_end + 1):
-            page_bar.append(i)
-        context['page_bar'] = page_bar
-
-        return HttpResponse(template.render(context, request))
 
 class AdminRedemptionCodeListView(View):
 
@@ -304,6 +260,52 @@ class AdminCouponCodeListView(View):
                     page_start = page_start - 1
         page_bar = []
         for i in range(page_start, page_end+1):
+            page_bar.append(i)
+        context['page_bar'] = page_bar
+
+        return HttpResponse(template.render(context, request))
+
+
+class UserLogListView(View):
+
+    def get(self, request, page=1):
+        if not request.user.is_authenticated:
+            response = redirect(reverse('user_login_view'))
+            response['Location'] += '?redirect_uri=' + request.path
+            return response
+
+        context = {}
+        result = Log.objects.filter(user=request.user)
+        context['page_name'] = 'Log List'
+
+        paginator = Paginator(result, 10)
+        page_obj = paginator.get_page(page)
+
+        template = loader.get_template('dashboard/user/log_list.html')
+
+        context['site_name'] = settings.SITE_NAME
+        context['is_authenticated'] = True
+        context['is_superuser'] = request.user.is_superuser
+        context['is_teacher'] = request.session.get('is_teacher')
+        context['username'] = request.user.name
+        context['hide_welcome'] = True
+        context['page_obj'] = page_obj
+
+        page_start = page_obj.number
+        page_bar_num = 5
+        for i in range(int(page_bar_num / 2)):
+            if page_start - 1 > 0:
+                page_start = page_start - 1
+        page_end = page_start
+        for i in range(int(page_bar_num / 2) * 2):
+            if page_end + 1 <= page_obj.paginator.num_pages:
+                page_end = page_end + 1
+        if page_end - page_start < int(page_bar_num / 2) * 2:
+            for i in range(int(page_bar_num / 2) * 2):
+                if page_start - 1 >= 1 and page_end - page_start < int(page_bar_num / 2) * 2:
+                    page_start = page_start - 1
+        page_bar = []
+        for i in range(page_start, page_end + 1):
             page_bar.append(i)
         context['page_bar'] = page_bar
 
