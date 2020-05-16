3a88bdcbae843a890fa5f697f8c264f51984dbb9
thinkray <3523177+thinkray@users.noreply.github.com>
2020-05-16 16:23:42 +0800

    Implement the UserLogListView and the AdminLogListView

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     #path('<int:question_id>/vote/', views.vote, name='vote'),
     path('bill/', views.UserBillListView.as_view(), name='bill_list_view'),
     path('bill/<int:page>', views.UserBillListView.as_view(), name='bill_list_view_page'),
+    path('log/', views.UserLogListView.as_view(), name='user_log_list_view'),
+    path('log/<int:page>', views.UserLogListView.as_view(), name='user_log_list_view_page'),
     path('admin/user/', views.AdminUserListView.as_view(), name='admin_user_list_view'),
     path('admin/user/<int:page>', views.AdminUserListView.as_view(), name='admin_user_list_view_page'),
     path('admin/bill/', views.AdminBillListView.as_view(), name='admin_bill_list_view'),
@@ -21,4 +23,6 @@ urlpatterns = [
     path('admin/code/coupon/<int:page>', views.AdminCouponCodeListView.as_view(), name='admin_coupon_code_list_view_page'),
     path('admin/code/redemption/', views.AdminRedemptionCodeListView.as_view(), name='admin_redemption_code_list_view'),
     path('admin/code/redemption/<int:page>', views.AdminRedemptionCodeListView.as_view(), name='admin_redemption_code_list_view_page'),
+    path('admin/log/', views.AdminLogListView.as_view(), name='admin_log_list_view'),
+    path('admin/log/<int:page>', views.AdminLogListView.as_view(), name='admin_log_list_view_page'),
 ]
