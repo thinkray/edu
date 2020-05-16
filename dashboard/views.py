4abac4501e8476a37b166fbdebbb68519f537348
thinkray <3523177+thinkray@users.noreply.github.com>
2020-05-16 22:12:41 +0800

    Implement the AdminBalanceListView

--- dashboard/views.py
+++ dashboard/views.py
@@ -12,6 +12,56 @@ from finance.models import Bill, CouponCode, RedemptionCode
 from site_log.models import Log
 
 
+class AdminBalanceListView(View):
+
+    def get(self, request, page=1):
+        if not request.user.is_authenticated:
+            response = redirect(reverse('user_login_view'))
+            response['Location'] += '?redirect_uri=' + request.path
+            return response
+
+        if not request.user.is_superuser:
+            raise PermissionDenied()
+
+        context = {}
+        result = User.objects.all()
+        context['page_name'] = 'Balance List'
+
+        paginator = Paginator(result, 10)
+        page_obj = paginator.get_page(page)
+
+        template = loader.get_template('dashboard/admin/balance_list.html')
+
+        context['site_name'] = settings.SITE_NAME
+        context['is_authenticated'] = True
+        context['is_superuser'] = request.user.is_superuser
+        context['is_teacher'] = request.session.get('is_teacher')
+        context['name'] = request.user.name
+        context['username'] = request.user.username
+        context['hide_welcome'] = True
+        context['page_obj'] = page_obj
+
+        page_start = page_obj.number
+        page_bar_num = 5
+        for i in range(int(page_bar_num / 2)):
+            if page_start - 1 > 0:
+                page_start = page_start - 1
+        page_end = page_start
+        for i in range(int(page_bar_num / 2) * 2):
+            if page_end + 1 <= page_obj.paginator.num_pages:
+                page_end = page_end + 1
+        if page_end - page_start < int(page_bar_num / 2) * 2:
+            for i in range(int(page_bar_num / 2) * 2):
+                if page_start - 1 >= 1 and page_end - page_start < int(page_bar_num / 2) * 2:
+                    page_start = page_start - 1
+        page_bar = []
+        for i in range(page_start, page_end + 1):
+            page_bar.append(i)
+        context['page_bar'] = page_bar
+
+        return HttpResponse(template.render(context, request))
+
+
 class UserBillListView(View):
 
     def get(self, request, page=1):
