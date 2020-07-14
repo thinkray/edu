8172c09353f53c1c4ec3e6be69ff17c872c526c5
heimoshuiyu <heimoshuiyu@gmail.com>
2020-07-14 22:43:56 +0800

    change to sqlite3

--- edu/settings.py
+++ edu/settings.py
@@ -94,15 +94,8 @@ AUTH_USER_MODEL = 'account.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_edu',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
